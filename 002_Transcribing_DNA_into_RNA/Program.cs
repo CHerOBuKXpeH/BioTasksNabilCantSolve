22ad80a2212fd247f3e5a635c3ecb2d6a050f218
CHerOBuK_XpeH <FadelSux@outlook.com>
2025-06-02 06:04:12 +0300

    002 Transcribing DNA into RNA

---
+++ 002_Transcribing_DNA_into_RNA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using FileHelper;
+
+var dna = await "task.txt".ReadTextFromHere();
+
+var sb = new StringBuilder();
+
+foreach (var nucleotide in dna)
+{
+    sb.Append(nucleotide != 'T' ? nucleotide : 'U');
+}
+
+var result = sb.ToString();
+
+Console.WriteLine(result);
---
+++ FileHelper/ChicoFileHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace FileHelper;
+
+public static class ChicoFileHelper
+{
+    public static async Task<string> ReadTextFromHere(this string path)
+    {
+        var result = string.Empty;
+        
+        try
+        {
+            result = await File.ReadAllTextAsync(path);
+        }
+        catch (Exception e) when (e is IOException or UnauthorizedAccessException)
+        {
+            Console.WriteLine("Could not read file");
+            Environment.Exit(-1);
+        }
+        
+        return result;
+    }
+}
