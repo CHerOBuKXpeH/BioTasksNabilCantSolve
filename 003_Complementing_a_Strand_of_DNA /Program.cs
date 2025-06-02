cd3a7aaa9b8e619a223e9ce20a54da51eb04f18d
CHerOBuKXpeH <sisystemscomp@outlook.com>
2025-06-02 16:03:52 +0300

    003 Complementing a Strand of DNA

---
+++ 003_Complementing_a_Strand_of_DNA /Program.cs
@@ -0,0 +1,17 @@
+﻿using FileHelper;
+
+var dna = await "task.txt".ReadTextFromHere();
+
+dna.Reverse()
+    .Select(x => x switch 
+    {
+        'A' => 'T',
+        'T' => 'A',
+        'G' => 'C',
+        'C' => 'G',
+        _ => '\0'
+    })
+    .ToList()
+    .ForEach(Console.Write);
+
+
