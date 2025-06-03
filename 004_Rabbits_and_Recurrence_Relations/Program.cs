747ec58b3596616a5da05b1df1fc5df1836eeb73
CHerOBuKXpeH <sisystemscomp@outlook.com>
2025-06-03 10:03:43 +0300

    1. Added an SLN file 2. 004 Rabbits and Recurrence Relations

---
+++ 004_Rabbits_and_Recurrence_Relations/Program.cs
@@ -0,0 +1,34 @@
+﻿var recNumber = Console.ReadLine();
+
+if (!int.TryParse(recNumber, out var numberParsed))
+{
+    Console.WriteLine("Not an integer");
+    Environment.Exit(-1);
+}
+
+var kString = Console.ReadLine();
+
+if (!int.TryParse(kString, out var kParsed))
+{
+    Console.WriteLine("Not an integer");
+    Environment.Exit(-1);
+}
+
+Console.WriteLine(RecurrentRelation(numberParsed, kParsed));
+return;
+
+long RecurrentRelation(int n, int k) 
+{
+    var prev = 0L;
+    var next = 1L;
+    
+    for (var i = 1; i < n; i++)
+    {
+        var sum = prev * k + next;
+        prev = next;
+        next = sum;
+    }
+    
+    return next;
+}
+
