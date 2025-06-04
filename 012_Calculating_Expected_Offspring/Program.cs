c4e69a5250fc5a5eec2a4443511dae9ce4b0f80e
CHerOBuKXpeH <sisystemscomp@outlook.com>
2025-06-04 03:52:32 +0300

    012 Calculating Expected Offspring

---
+++ 012_Calculating_Expected_Offspring/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(CalculateDominantOffspring([17976,16436,18791,19132,17596,18797]));
+return;
+
+static double CalculateDominantOffspring(int[] couples)
+{
+    double[] probabilities = [1.0, 1.0, 1.0, 0.75, 0.5, 0.0];
+        
+    double expectedOffspring = 0;
+    
+    for (var i = 0; i < 6; i++)
+    {
+        expectedOffspring += couples[i] * 2 * probabilities[i];
+    }
+        
+    return expectedOffspring;
+}
