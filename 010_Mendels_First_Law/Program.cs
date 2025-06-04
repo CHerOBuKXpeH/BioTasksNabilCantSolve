8a9363556275ba2c023267779ede92f613a0b126
CHerOBuKXpeH <sisystemscomp@outlook.com>
2025-06-04 03:17:36 +0300

    010 Mendel's First Law

---
+++ 010_Mendels_First_Law/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(CalculateDominantProbability(29,21, 17));
+return;
+
+static double CalculateDominantProbability(int k, int m, int n)
+{
+    var total = k + m + n;
+    var totalPairs = total * (total - 1) / 2.0;
+    
+    var dominantPairs = 
+        k * (k - 1) / 2.0 * 1.0 +           
+        k * m * 1.0 +                       
+        k * n * 1.0 +                       
+        m * (m - 1) / 2.0 * 0.75 +         
+        m * n * 0.5 +                       
+        n * (n - 1) / 2.0 * 0.0;         
+
+    return dominantPairs / totalPairs;
+}
